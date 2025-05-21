531772e22db1143585271495b6fba6288b8af546
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-05-21 16:01:08 -0300

    Refactor Register component for improved layout and consistency in styling

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,24 +4,26 @@ import logo from "@/public/logo.png";
 import Link from "next/link";
 import { MoveLeft } from "lucide-react";
 
-
 export default function Register() {
     return (
-        <div className="bg-black/90 w-full h-screen flex items-center justify-center">
-            <div className="flex flex-col items-center justify-center w-[40vw] bg-white/10 backdrop-blur-md border border-white/20 p-4 rounded shadow-lg">
-                <div>
-                    <Link href="/login" className="absolute top-4 left-4 flex gap-1 text-blue-500"><MoveLeft />voltar</Link>
-                    <Image src={logo} alt="logo" width={200} />
+        <div className="bg-black/90 w-full min-h-screen flex items-center justify-center px-4">
+            <div className="w-full max-w-md flex flex-col items-center justify-center bg-white/10 backdrop-blur-md border border-white/20 p-6 rounded-lg shadow-lg">
+                <div className="w-full relative flex items-center justify-center mb-4">
+                    <Link href="/login" className="absolute left-0 top-0 flex items-center gap-1 text-blue-500">
+                        <MoveLeft /> Voltar
+                    </Link>
+                    <Image src={logo} alt="logo" width={150} />
                 </div>
-                <h1 className="text-2xl uppercase mb-2 text-white">Registrar</h1>
-                <form action="" className="flex flex-col items-center justify-center w-full gap-4">
-                    <Input className="w-[50%] text-white" type="text" placeholder="Nome" />
-                    <Input className="w-[50%] text-white" type="email" placeholder="Email" />
-                    <Input className="w-[50%] text-white" type="password" placeholder="Senha" />
-                    <Input className="w-[50%] text-white" type="password" placeholder="Confirmar Senha" />
-                    <button className="bg-blue-500 px-4 py-2 rounded text-white cursor-pointer">Registrar</button>
+                <h1 className="text-2xl uppercase mb-4 text-white text-center">Registrar</h1>
+                <form className="flex flex-col items-center justify-center w-full gap-4">
+                    <Input className="w-full text-white" type="text" placeholder="Nome" />
+                    <Input className="w-full text-white" type="email" placeholder="Email" />
+                    <Input className="w-full text-white" type="password" placeholder="Senha" />
+                    <button className="w-full bg-blue-500 px-4 py-2 rounded text-white hover:bg-blue-600 transition">
+                        Registrar
+                    </button>
                 </form>
             </div>
         </div>
-    )
+    );
 }
